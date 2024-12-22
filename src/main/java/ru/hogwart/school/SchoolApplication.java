304f36fd90ba2acf6c728ca45ea13f6059326245
SFS76 <sfs76@yandex.ru>
2024-12-22 18:23:46 +0400

    111

---
+++ src/main/java/ru/hogwart/school/SchoolApplication.java
@@ -0,0 +1,14 @@
+package ru.hogwart.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class SchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApplication.class, args);
+	}
+
+}
