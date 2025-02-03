b526771b1cd58ee35fa6faa9dd9e03d9e5e57f95
SFS76 <sfs76@yandex.ru>
2025-02-03 20:29:37 +0400

    Merge remote-tracking branch 'origin/slave1' into slave1

--- scripts421.sql
+++ scripts421.sql
@@ -1,12 +1,12 @@
-//Возраст студента не может быть меньше 16 лет.
+--Возраст студента не может быть меньше 16 лет.
 ALTER TABLE student ADD CONSTRAINT age_constraint CHECK (age >= 16);
 
-//Имена студентов должны быть уникальными и не равны нулю.
+--Имена студентов должны быть уникальными и не равны нулю.
 ALTER TABLE student add CONSTRAINT unuq_name UNIQUE (name);
 ALTER TABLE student alter column name set not null;
 
-//Пара “значение названия” - “цвет факультета” должна быть уникальной.
+--Пара “значение названия” - “цвет факультета” должна быть уникальной.
 ALTER TABLE faculty add CONSTRAINT unuq UNIQUE (name, color);
 
-//При создании студента без возраста ему автоматически должно присваиваться 20 лет.
+--При создании студента без возраста ему автоматически должно присваиваться 20 лет.
 ALTER TABLE student alter column age set default 20;
