36aad19d1c566f936e62d2d154efa5ed075bd57e
SFS76 <sfs76@yandex.ru>
2025-02-09 14:44:54 +0400

    Merge remote-tracking branch 'origin/slave1' into slave1

---
+++ src/main/java/ru/hogwart/school/controller/InfoController.java
@@ -0,0 +1,12 @@
+package ru.hogwart.school.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/port")
+public class InfoController {
+    @Value("${server.port}")
+    private String port;
+}
